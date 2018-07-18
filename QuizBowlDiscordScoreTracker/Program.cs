44721b353c36403c73b71a926a4e55984121cc42
alopezlago <alopezlago@cs.hmc.edu>
2018-07-17 22:40:09 -0700

    Add project files.

---
+++ QuizBowlDiscordScoreTracker/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace QuizBowlDiscordScoreTracker
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
