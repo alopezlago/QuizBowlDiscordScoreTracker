ca6ff91c3d14795e1f5e1be5735d328b9cfce8a6
Albert Zhang <alchzh@gmail.com>
2020-07-31 11:14:01 -0400

    don't connect if no query

--- QuizBowlDiscordScoreTracker/Web/wwwroot/js/main.js
+++ QuizBowlDiscordScoreTracker/Web/wwwroot/js/main.js
@@ -1,34 +1,39 @@
 "use strict";
 
-var connection = new signalR.HubConnectionBuilder().withUrl("/hub").build();
 var nameDisplay = document.getElementById("name");
-var buzzSound = document.getElementById("buzzSound");
-
-var shouldEmitSound = true;
 
 function removeBarrier() {
     document.getElementById("interactBarrier").style.display = "none";
 }
 
-connection.on("PlayerBuzz", function (name) {
-    document.body.style.backgroundColor = "red";
-    nameDisplay.textContent = name;
+if (window.location.search.length <= 1) {
+    nameDisplay.textContent = "No channel specified."
+} else {
+    var connection = new signalR.HubConnectionBuilder().withUrl("/hub").build();
+    var buzzSound = document.getElementById("buzzSound");
 
-    if (shouldEmitSound) {
-        buzzSound.play();
-    }
+    var shouldEmitSound = true;
 
-    shouldEmitSound = false;
-});
+    connection.on("PlayerBuzz", function (name) {
+        document.body.style.backgroundColor = "red";
+        nameDisplay.textContent = name;
 
-connection.on("Clear", function () {
-    document.body.style.backgroundColor = "green";
-    nameDisplay.textContent = "";
-    shouldEmitSound = true;
-});
+        if (shouldEmitSound) {
+            buzzSound.play();
+        }
 
-connection.start().then(function () {
-    connection.invoke("AddToChannel", window.location.search.substring(1));
-}).catch(function (err) {
-    return console.error(err.toString());
-});
+        shouldEmitSound = false;
+    });
+
+    connection.on("Clear", function () {
+        document.body.style.backgroundColor = "green";
+        nameDisplay.textContent = "";
+        shouldEmitSound = true;
+    });
+
+    connection.start().then(function () {
+        connection.invoke("AddToChannel", window.location.search.substring(1));
+    }).catch(function (err) {
+        return console.error(err.toString());
+    });
+}
