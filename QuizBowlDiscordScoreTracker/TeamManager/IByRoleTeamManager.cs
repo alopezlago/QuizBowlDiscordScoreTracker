57d1ea78a4e570e1f7b074877779b2fcefd7a310
Michal Knapík <56478032+mk625sn@users.noreply.github.com>
2020-12-12 22:36:41 +0100

    -- public

--- QuizBowlDiscordScoreTracker/TeamManager/IByRoleTeamManager.cs
+++ QuizBowlDiscordScoreTracker/TeamManager/IByRoleTeamManager.cs
@@ -5,7 +5,7 @@ using System.Threading.Tasks;
 
 namespace QuizBowlDiscordScoreTracker.TeamManager
 {
-    public interface IByRoleTeamManager
+    interface IByRoleTeamManager
     {
         public void ReloadTeamRoles(out string message);
     }
